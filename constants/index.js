4b715b152894a5380746414e3837e8ccfc968aae
Nazmul <nazmul.hasan1.61803@gmail.com>
2023-01-02 17:20:53 +0600

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,119 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    id: "social-media-4",
+    icon: '/github.svg',
+    link: "https://github.com/Nazmul6180"
+  },
+  {
+    id: "social-media-1",
+    icon: '/instagram.svg',
+    link: "https://www.instagram.com/identicalnazmul/",
+  },
+  {
+    id: "social-media-2",
+    icon: '/facebook.svg',
+    link: "https://www.facebook.com/identical.nazmul",
+  },
+  {
+    id: "social-media-3",
+    icon: '/twitter.svg',
+    link: "https://twitter.com/8Nazmul",
+  },
+  {
+    id: "social-media-5",
+    icon: '/linkedin.svg',
+    link: "https://linkdin.com",
+  },
+];
+// export const socialMedia = [
+//   {
+//     name: "instagram",
+//     url: "/instagram.svg",
+//     link: "https://www.instagram.com/identicalnazmul/",
+//   },
+//   {
+//     name: " ",
+//     url: facebook,
+//     link: "https://www.facebook.com/identical.nazmul",
+//   },
+//   {
+//     name: "social-media-3",
+//     url: twitter,
+//     link: "https://twitter.com/8Nazmul",
+//   },
+//   {
+//     name: "social-media-4",
+//     url: github,
+//     link: "https://github.com/Nazmul6180"
+//   },
+// ];
